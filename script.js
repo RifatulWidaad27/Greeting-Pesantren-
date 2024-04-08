721207a957fe7d3204b6049b04f70f4fbc0d3ea4
RifatulWidaad27 <166233838+RifatulWidaad27@users.noreply.github.com>
2024-04-08 21:39:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const nameInput = document.getElementById("name");
 const downloadBtn = document.getElementById("download-btn");
 
 const image = new Image();
-image.src = "design.png";
+image.src = "Main.png";
 image.onload = function () {
   drawImage();
 };
